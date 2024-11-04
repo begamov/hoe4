529b1b35d1b3672e256d5668589fa119f89c0b35
begamov <s.begamov@newuu.uz>
2024-11-04 20:01:14 +0500

    just chillin'

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "problems.h"
+using namespace std;
+int main() {
+    double num, num1, num2;
+    cin >> num>> num1 >>num2 ;
+    //exampleFunction();
+    //cout<< num<<" "<<num1;
+    /*swap_values(num, ans);
+    cout<< num << " " << ans;*/
+    //cout<<multiply(num, ans)<<endl;
+    //cout<<decimalToBinary(num);
+    cout<<midValue(num, num1, num2) << endl;
+    return 0;
+}
